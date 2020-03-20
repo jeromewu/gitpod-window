c33bc632871e8c526d35fee903e35f1e3bd3b05a
jeromewu <jeromewus@gmail.com>
2020-03-20 04:40:36 +0000

    Add browser action

--- background.js
+++ background.js
@@ -1,5 +1,6 @@
 /*
  * @ref: https://developer.chrome.com/extensions/commands
+ * @ref: https://developer.chrome.com/extensions/browserAction
  */
 
 chrome.commands.onCommand.addListener(function(command) {});
