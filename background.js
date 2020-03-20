b24a3a17ca2beb762597b2155fa0a42c417ff53a
jeromewu <jeromewus@gmail.com>
2020-03-20 05:00:40 +0000

    Add url query

--- background.js
+++ background.js
@@ -5,8 +5,11 @@
 
 chrome.commands.onCommand.addListener(function(command) {});
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.windows.create({
-    url: 'https://www.gitpod.io/',
-    type: 'popup',
+  chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs) {
+    var url = tabs[0].url;
+    chrome.windows.create({
+      url: `https://.gitpod.io/#${url}`,
+      type: 'popup',
+    });
   });
 });
